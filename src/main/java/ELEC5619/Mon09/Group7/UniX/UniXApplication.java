e8cbc5e8ff1d8d882c3de13e76f7a0a5fb6383fb
Ayushmaan Tomar <atom5594@uni.sydney.edu.au>
2023-08-28 10:07:21 +1000

    Added Gradle Source Files

---
+++ src/main/java/ELEC5619/Mon09/Group7/UniX/UniXApplication.java
@@ -0,0 +1,13 @@
+package ELEC5619.Mon09.Group7.UniX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniXApplication.class, args);
+	}
+
+}
