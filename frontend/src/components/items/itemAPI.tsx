3028dd119a31d2ca7b065ceab2d691f0337e9ae8
Ayushmaan Tomar <atom5594@uni.sydney.edu.au>
2023-09-19 00:13:05 +1000

    implemented item API

---
+++ frontend/src/components/items/itemAPI.tsx
@@ -0,0 +1,97 @@
+import { Item } from './listings';
+
+const baseUrl = 'http://localhost:4000';
+const url = `${baseUrl}/products`;
+
+function translateStatusToErrorMessage(status: number) {
+  switch (status) {
+    case 401:
+      return 'Please login again.';
+    case 403:
+      return 'You do not have permission to view the project(s).';
+    default:
+      return 'There was an error retrieving the project(s). Please try again.';
+  }
+}
+
+function checkStatus(response: any) {
+  if (response.ok) {
+    return response;
+  } else {
+    const httpErrorInfo = {
+      status: response.status,
+      statusText: response.statusText,
+      url: response.url,
+    };
+    console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+    let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+    throw new Error(errorMessage);
+  }
+}
+
+function parseJSON(response: Response) {
+  return response.json();
+}
+
+// eslint-disable-next-line
+function delay(ms: number) {
+  return function (x: any): Promise<any> {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
+}
+
+function convertToProjectModels(data: any[]): Item[] {
+  let products: Item[] = data;
+  return products;
+}
+
+function convertToProjectModel(item: any): Item {
+  return new Item(item);
+}
+
+const productAPI = {
+  get(page = 1, limit = 12) {
+    return (
+      fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+        // .then(delay(2000))
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(convertToProjectModels)
+        .catch((error: TypeError) => {
+          console.log('log client error ' + error);
+          throw new Error(
+            'There was an error retrieving the projects. Please try again.'
+          );
+        })
+    );
+  },
+
+  find(id: number): Promise<Item> {
+    return fetch(`${url}/${id}`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(convertToProjectModel);
+  },
+
+  put(project: Item) {
+    return fetch(`${url}/${project.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(project),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(delay(2000))
+      .then(checkStatus)
+      .then(parseJSON)
+      .catch((error: TypeError) => {
+        console.log('log client error ' + error);
+        throw new Error(
+          'There was an error updating the project. Please try again.'
+        );
+      });
+  },
+};
+
+export { productAPI };
