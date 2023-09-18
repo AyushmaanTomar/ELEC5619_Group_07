73a395f2f1a98314973f7fd2498dfda0aa6d1042
Ayushmaan Tomar <atom5594@uni.sydney.edu.au>
2023-09-19 00:13:28 +1000

    implemented item details

---
+++ frontend/src/components/items/itemDetail.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Item } from './listings';
+
+interface ProductDetailProps {
+  project: Item;
+}
+export default function ProductDetail({ project }: ProductDetailProps) {
+  return (
+    <div className="">
+      <div className="">
+        <div className="card card-bordered bg-gray-200 w-96 relative">
+          <figure>
+            <img
+              className=""
+              src={project.imageUrl}
+              alt={project.name}
+              width={500}
+              height={300}
+            />
+          </figure>
+          <section className="card-body">
+            <h3 className="card-title mb-4 text-base">
+              <strong>{project.name}</strong>
+            </h3>
+            <p className=" text-gray-500 mb-4">{project.description}</p>
+            <p className=" text-gray-500">
+              <span className="text-gray-800 font-semibold">Price: </span>$
+              {project.price.toLocaleString()}
+            </p>
+
+            <p className=" text-gray-500">
+              <span className="text-gray-800 font-semibold">Seller: </span>{' '}
+              {project.seller}
+            </p>
+            <p className=" text-gray-500">
+              <span className="text-gray-800 font-semibold">Quality: </span>{' '}
+              {project.isActive ? 'active' : 'inactive'}
+            </p>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
