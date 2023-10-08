52641453c6a611565c7ff98bb2e5f1defc62e538
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2023-10-09 09:47:58 +1100

    Fixed naming issue

---
+++ frontend/src/components/items/listingPage.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { productAPI } from './itemAPI';
+import ProductDetail from './itemDetail';
+import { Item } from './listings';
+import { useParams } from 'react-router-dom';
+
+function ProductPage(props: any) {
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState<Item | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const params = useParams();
+  const id = Number(params.id);
+
+  useEffect(() => {
+    setLoading(true);
+    productAPI
+      .find(id)
+      .then((data) => {
+        setProject(data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setError(e);
+        setLoading(false);
+      });
+  }, [id]);
+
+  return (
+    <div>
+      <>
+        <h2 className="text-2xl font-extrabold my-8">Product Detail</h2>
+
+        {loading && (
+          <div className="center-page">
+            <span className="spinner primary"></span>
+            <p>Loading...</p>
+          </div>
+        )}
+
+        {error && (
+          <div className="">
+            <div className="">
+              <section>
+                <p>
+                  <span className="icon-alert inverse "></span> {error}
+                </p>
+              </section>
+            </div>
+          </div>
+        )}
+
+        {project && <ProductDetail project={project} />}
+      </>
+    </div>
+  );
+}
+
+export default ProductPage;
