b5d7dca72b5355d7c1bcec4942e4386460833fe8
Taizeen <taizeenmunir18@gmail.com>
2023-10-16 10:09:10 +1100

    fixed routing bug

--- frontend/src/components/usermanagement/profileHooks.tsx
+++ frontend/src/components/usermanagement/profileHooks.tsx
@@ -4,7 +4,7 @@ import { userAPI } from './profileAPI';
 
 export function useProfile() {
   const [page, setPage] = useState(0);
-  let queryInfo = useQuery(['projects', page], () => userAPI.get(page + 1), {
+  let queryInfo = useQuery(['profile', page], () => userAPI.get(page + 1), {
     keepPreviousData: true,
     // staleTime: 5000,
   });
