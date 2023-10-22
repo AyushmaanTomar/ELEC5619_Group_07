7cb00b94b184a94891fbbfd0c7ed05e58594f8b4
Nicolasmages <92356838+Nicolasmages@users.noreply.github.com>
2023-10-23 10:29:02 +1100

    Update

--- backend/Group7/src/main/java/ELEC5619/Group7/controller/ItemController.java
+++ backend/Group7/src/main/java/ELEC5619/Group7/controller/ItemController.java
@@ -2,6 +2,8 @@ package ELEC5619.Group7.controller;
 
 import ELEC5619.Group7.service.ItemService;
 import ELEC5619.Group7.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class ItemController {
 
