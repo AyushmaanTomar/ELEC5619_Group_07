8637edd928b9170e5f4d895ac558b25de27e75af
Alexis Lee <alee6817@uni.sydney.edu.au>
2023-10-25 23:38:43 +1100

    changed controllers to have BADREQ, UNAUTH

--- backend/Group7/src/main/java/ELEC5619/Group7/controller/UserController.java
+++ backend/Group7/src/main/java/ELEC5619/Group7/controller/UserController.java
@@ -3,50 +3,69 @@ package ELEC5619.Group7.controller;
 import ELEC5619.Group7.entity.User;
 import ELEC5619.Group7.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users") // This is to have a common prefix for user related endpoints.
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "info", method = RequestMethod.GET)
-    public String info() {
-        return "The application is up...";
+    @GetMapping("/info")
+    public ResponseEntity<String> info() {
+        return new ResponseEntity<>("The application is up...", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "createsUser", method = RequestMethod.POST)
-    public String createUser(
-            @RequestBody User user) {
-        return userService.createStudent(user);
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        String result = userService.createStudent(user);
+        if(result.equals("success")) {
+            return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Failed to create user", HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @RequestMapping(value = "readsUser", method = RequestMethod.GET)
-    public List<User> readUser() {
-        return userService.readUser();
+    @GetMapping
+    public ResponseEntity<List<User>> readUser() {
+        return new ResponseEntity<>(userService.readUser(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "updateUser", method = RequestMethod.PUT)
-    public String updateUser(@RequestBody User user) {
-        return userService.updateUser(user);
+    @PutMapping
+    public ResponseEntity<String> updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+        return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "deleteUser", method = RequestMethod.DELETE)
-    public String deleteStudent(@RequestBody User user) {
-        return userService.deleteUser(user);
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestBody User user) {
+        userService.deleteUser(user);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String loginUser(@RequestBody User user) {
-        return userService.authenticateUser(user);
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
+        String result = userService.authenticateUser(user);
+        if(result.equals("success")) {
+            return new ResponseEntity<>("Login successful", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
+        }
     }
 
-    @RequestMapping(value = "changePassword", method = RequestMethod.PUT)
-    public String changePassword(@RequestBody User user, @RequestParam String newPassword) {
-        return userService.changePassword(user, newPassword);
+    @PutMapping("/changePassword")
+    public ResponseEntity<String> changePassword(@RequestBody User user, @RequestParam String newPassword) {
+        String result = userService.changePassword(user, newPassword);
+        if(result.equals("success")) {
+            return new ResponseEntity<>("Password changed successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to change password", HttpStatus.BAD_REQUEST);
+        }
     }
-
 }
